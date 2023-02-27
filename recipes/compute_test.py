0d8867f3e65ce882ed7e6b9be7052640e829c28a
admin <admin>
2023-02-27 14:11:05 +0100

    Saved recipe: MLOPS_TUTORIAL_FOR_MUSTAPHA_BIG_BOSS_MUSTAPHA.compute_test

---
+++ recipes/compute_test.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+
+# Read recipe inputs
+transactions_MES_filtered = dataiku.Dataset("transactions_MES_filtered")
+transactions_MES_filtered_df = transactions_MES_filtered.get_dataframe()
+
+
+# Compute recipe outputs from inputs
+# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
+# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
+
+test_df = transactions_MES_filtered_df # For this sample code, simply copy input to output
+
+
+# Write recipe outputs
+test = dataiku.Dataset("test")
+test.write_with_schema(test_df)
